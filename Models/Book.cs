ef6d5ad2fd811b21b829fd9b5f5095fcfab70483
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-07-29 18:37:09 -0500

    feat: Exportar Historial de Libros de un usuario

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,10 +9,10 @@ namespace Backend.Models
         public string Title { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
-        public Date PublicationDate { get; set; }
+        public DateTime PublicationDate { get; set; }
         public int CopiesAvailable { get; set; }
         public string Status { get; set; }
         [JsonIgnore]
-        public List<Loan>Loans { get; set; }
+        public List<Loan> Loans { get; set; }
     }
 }
